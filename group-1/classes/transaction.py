6d195af2074820cb3b40a29e65c171fbf7943d03
alial95 <alistair_j_cooper@hotmail.com>
2020-10-08 15:31:24 +0000

    fixed one problem

--- group-1/classes/transaction.py
+++ group-1/classes/transaction.py
@@ -1,9 +1,9 @@
 class Transaction:
-        def __init__(self, total, customer_name, date, location):
-            self.total = total
-            self.customer_name = customer_name
-            self.date = date
-            self.location = location
-        def __repr__(self):
-            return f'Customer name is {self.customer_name}.'
+    def __init__(self, total, customer_name, date, location):
+        self.total = total
+        self.customer_name = customer_name
+        self.date = date
+        self.location = location
+    def __repr__(self):
+        return f'Customer name is {self.customer_name}.'
 
