77d0fc491060e9390e3f45546e6ba0c87354a36a
alial95 <alistair_j_cooper@hotmail.com>
2020-10-19 12:15:04 +0000

    put lambda trigger in yml file

--- group-1/extract/extract.py
+++ group-1/extract/extract.py
@@ -7,29 +7,31 @@ queue_url = 'https://sqs.eu-west-1.amazonaws.com/579154747729/extract-to-load'
 
 def start(event, context):
     print('running extract lambda')
-    # key = event['Records'][0]['s3']['object']['key']
-    # bucket = event['Records'][0]['s3']['bucket']['name']
+    # get the csv and bucket name for read_csv function
+    key = event['Records'][0]['s3']['object']['key']
+    bucket = event['Records'][0]['s3']['bucket']['name']
 
     print('TEST STATEMENT.')
     print('second test')
 
-    sqs = boto3.client('sqs')
-    s3 = boto3.client('s3')
-    response = s3.list_objects(Bucket='cafe-transactions')
-    first_step = response['Contents']
-    keys = []
-    for key in first_step:
-        # print(key['Key'])
-        keys.append(key['Key'])
-    test_key = keys[0]
-    bucket = 'cafe-transactions'
-    path = f's3://{bucket}/{test_key}'
+    
+    # s3 = boto3.client('s3')
+    # response = s3.list_objects(Bucket='cafe-transactions')
+    # first_step = response['Contents']
+    # keys = []
+    # for key in first_step:
+    #     # print(key['Key'])
+    #     keys.append(key['Key'])
+    # test_key = keys[0]
+    # bucket = 'cafe-transactions'
+    path = f's3://{bucket}/{key}'
     test_df = pd.read_csv(path, names=['date', 'location', 'customer_name', 'basket', 'pay_amount', 'payment_method', 'ccn'])
     # dataframes = []
     # for csv in keys:
     #     path = f's3://{bucket}/{csv}'
     #     df = pd.read_csv(path, names=['date', 'location', 'customer_name', 'basket', 'pay_amount', 'payment_method', 'ccn'])
     #     dataframes.append(df)
+    # fill the raw_transactions list with json objects
     raw_transactions = []
     for i in range(1, len(test_df)):
         transaction = {
@@ -51,9 +53,9 @@ def start(event, context):
 
 
 
-    # for dataframe in dataframes:
-    
-
+    # connect to sqs
+    sqs = boto3.client('sqs')
+    # send the message
     response = sqs.send_message(
         QueueUrl = queue_url_1,
         MessageBody = json_data
