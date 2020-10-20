da54be872ddfb0e2e4a85286dfb0d7bf4e71b848
alial95 <alistair_j_cooper@hotmail.com>
2020-10-20 05:53:57 +0000

    setting up test trigger

--- group-1/extract/extract.py
+++ group-1/extract/extract.py
@@ -8,23 +8,23 @@ queue_url = 'https://sqs.eu-west-1.amazonaws.com/579154747729/extract-to-load'
 def start(event, context):
     print('running extract lambda')
     # get the csv and bucket name for read_csv function
-    key = event['Records'][0]['s3']['object']['key']
-    bucket = event['Records'][0]['s3']['bucket']['name']
+    # key = event['Records'][0]['s3']['object']['key']
+    # bucket = event['Records'][0]['s3']['bucket']['name']
 
     print('TEST STATEMENT')
     print('second test')
 
     
-    # s3 = boto3.client('s3')
-    # response = s3.list_objects(Bucket='cafe-transactions')
-    # first_step = response['Contents']
-    # keys = []
-    # for key in first_step:
-    #     # print(key['Key'])
-    #     keys.append(key['Key'])
-    # test_key = keys[0]
-    # bucket = 'cafe-transactions'
-    path = f's3://{bucket}/{key}'
+    s3 = boto3.client('s3')
+    response = s3.list_objects(Bucket='cafe-transactions')
+    first_step = response['Contents']
+    keys = []
+    for key in first_step:
+        # print(key['Key'])
+        keys.append(key['Key'])
+    test_key = keys[0]
+    bucket = 'cafe-transactions'
+    path = f's3://{bucket}/{test_key}'
     test_df = pd.read_csv(path, names=['date', 'location', 'customer_name', 'basket', 'pay_amount', 'payment_method', 'ccn'])
     # dataframes = []
     # for csv in keys:
