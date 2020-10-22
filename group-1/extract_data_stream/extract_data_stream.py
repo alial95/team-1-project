74c5db9dc1a7a423c3f8ffab571eb629545a73c2
Joab <k1730992@kingston.ac.uk>
2020-10-22 15:57:14 +0100

    Began work od ds lambda, serverless updated 2.

---
+++ group-1/extract_data_stream/extract_data_stream.py
@@ -0,0 +1,33 @@
+import base64
+import json
+
+def start(event, context):
+    
+    output = []
+
+    for record in event['records'] :
+        print(record['recordId'])
+        decoded_data = base64.b64decode(record['data']).decode('utf-8')
+        print(decoded_data)
+        reading = json.loads(decoded_data)
+        print(reading)
+
+        output_record = {
+            'recordId': record['recordId'],
+            'result': 'Ok',
+            'data': base64.b64encode(json.dumps(reading).encode('UTF-8'))
+        }
+        output.append(output_record)
+
+    print('Processed {} records.'.format(len(event['records'])))
+
+    # connect to sqs
+    sqs = boto3.client('sqs')
+    # send the message
+    
+    response = sqs.send_message(
+        QueueUrl = queue_url_1,
+        MessageBody = json_data
+    )
+    
+    return {'records': output}
