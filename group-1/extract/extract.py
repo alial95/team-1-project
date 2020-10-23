043145a0ad0e473cf5470dad8995f37351a54604
Danesh Paul <dnpaul97@gmail.com>
2020-10-23 11:20:04 +0100

    print record count etc.

--- group-1/extract/extract.py
+++ group-1/extract/extract.py
@@ -10,28 +10,14 @@ def start(event, context):
     # get the csv and bucket name for read_csv function
     key = event['Records'][0]['s3']['object']['key']
     bucket = event['Records'][0]['s3']['bucket']['name']
+    print(json.dumps({'recordCount': }))
+    print(json.dumps({'fileName': key}))
 
-    print('TEST STATEMENT')
-    print('second test')
-
-    
-    # s3 = boto3.client('s3')
-    # response = s3.list_objects(Bucket='cafe-transactions')
-    # first_step = response['Contents']
-    # keys = []
-    # for key in first_step:
-    #     # print(key['Key'])
-    #     keys.append(key['Key'])
-    # test_key = keys[0]
-    # bucket = 'cafe-transactions'
     path = f's3://{bucket}/{key}'
     test_df = pd.read_csv(path, names=['date', 'location', 'customer_name', 'basket', 'pay_amount', 'payment_method', 'ccn'])
-    # dataframes = []
-    # for csv in keys:
-    #     path = f's3://{bucket}/{csv}'
-    #     df = pd.read_csv(path, names=['date', 'location', 'customer_name', 'basket', 'pay_amount', 'payment_method', 'ccn'])
-    #     dataframes.append(df)
-    # fill the raw_transactions list with json objects
+
+    print(json.dumps({'recordCount': len(test_df),'fileName': key})
+
     raw_transactions = []
     for i in range(1, len(test_df)):
         transaction = {
@@ -43,14 +29,8 @@ def start(event, context):
         }
         raw_transactions.append(transaction)
     json_data = json.dumps(raw_transactions) 
-    
-    # queue_url = sqs.get_queue_url(
-    #     QueueName='extract-to-load',
 
-    #     )
     queue_url_1 = 'https://sqs.eu-west-1.amazonaws.com/579154747729/extract-to-transform'
-
-
 
 
     # connect to sqs
