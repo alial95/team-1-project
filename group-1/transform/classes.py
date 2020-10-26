421a421e2ca63e848a3cc6cb6795edcea965b8bf
alial95 <alistair_j_cooper@hotmail.com>
2020-10-26 16:19:12 +0000

    added Transaction and Basket class

---
+++ group-1/transform/classes.py
@@ -0,0 +1,13 @@
+class Transaction:
+
+    def __init__(self, date, time, location, customer, total):
+        self.date = date
+        self.time = time
+        self.location = location
+        self.customer = customer
+        self.total = total
+
+class Basket:
+    def __init__(self, basket_item, price):
+        self.basket_item = basket_item
+        
--- group-1/transform/transform.py
+++ group-1/transform/transform.py
@@ -1,76 +1,44 @@
 import boto3
 import json
+from classes import Transaction, Basket
 
 sqs = boto3.client('sqs')
 
 def start(event, context):
      print('lambda running')
-    
+     transactions = []
+     baskets = []
      for record in event['Records']:
           json_data = json.loads(record['body'])
-          baskets = []
-          dates= []
-          location = []
-          names = []
-          total = []
           for transaction in json_data:
-               baskets.append(transaction['basket'])
-          for transaction in json_data:
-               dates.append(transaction['date'])
-               location.append(transaction['location'])
-               names.append(transaction['customer_name'])
-               total.append(transaction['total'])
-          print(json.dumps({
-               'Location' : location[0]
-          }))
-          calendar_day = []
-          time_of_day = []
-          for date in dates:
-               times = date[-5:]
-               calendar_date = date[:-5]
-               calendar_day.append(calendar_date)
-               time_of_day.append(times)     
-
+               transaction = Transaction(transaction['date'][:-5], transaction['date'][-5:], transaction['location'], transaction['customer_name'], transaction['total'])
+               transactions.append(transaction)
+               basket_items = transaction['basket'].split(',')
+               for item in basket_items:
+                    price = item[0][-5:]
+                    basket_item = item[0].strip(f'{price}').strip(' -')
+                    basket = Basket(basket_item, price)
+                    baskets.append(basket)
           cleaned_transactions = []
-          for i in range(1, len(dates)):
-             transaction = {
-                 'calendar_day': calendar_day[i],
-                 'time_of_day': time_of_day[i],
-                 'location': location[i],
-                 'customer_name': names[i],
-                 'total': total[i]
+          for transaction in transactions:
+             transaction_dict = {
+                 'calendar_day': transaction[0],
+                 'time_of_day': transaction[1],
+                 'location': transaction[2],
+                 'customer_name': transaction[3],
+                 'total': transaction[4]
              }
-             cleaned_transactions.append(transaction)
+             cleaned_transactions.append(transaction_dict)
           transactions_json = json.dumps(cleaned_transactions) 
           
-          baskets_1 = []
-          for basket in baskets:
-               print(json.dumps({
-                    'basket string' : basket
-               }))
-               test_basket = basket.split(',')
-               baskets_1.append(test_basket)
-
-          basket_items = []
-          prices = []
-          clean_basket_items = []
-          for item in baskets_1:
-              basket_test = item[0].split(',')
-              basket_items.append(basket_test)
-
-          for item in basket_items:
-              price = item[0][-5:]
-              basket_item = item[0].strip(f'{price}').strip(' -')
-              clean_basket_items.append(basket_item)
-              prices.append(price)
 
           clean_basket = []
-          for i in range(1, len(prices)):
-               basket = {
-                    'Basket_item': clean_basket_items[i],
-                    'Price': prices[i]
+          for basket in baskets:
+               basket_dict = {
+                    'Basket_item': basket[0],
+                    'Price': basket[1]
                }
-               clean_basket.append(basket)
+               clean_basket.append(basket_dict)
           basket_json = json.dumps(clean_basket)
 
           queue_url = 'https://sqs.eu-west-1.amazonaws.com/579154747729/g1-transform-to-load'
