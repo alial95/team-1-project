5000051f337f9bde12efaab68afb41e2bc16f57b
alial95 <alistair_j_cooper@hotmail.com>
2020-10-27 19:47:48 +0000

    changed syntax

--- group-1/extract_data_stream/extract_data_stream.py
+++ group-1/extract_data_stream/extract_data_stream.py
@@ -11,17 +11,17 @@ def start(event, context):
         print(record)
         decoded_data = base64.b64decode(record['kinesis']['data']).decode('utf-8')
         print(decoded_data)
-        reading = json.loads(decoded_data)
-        print(reading)
+        # reading = json.loads(decoded_data)
+        # print(reading)
 
-        output_record = {
-            'recordId': record['recordId'],
-            'result': 'Ok',
-            'data': base64.b64encode(json.dumps(reading).encode('UTF-8'))
-        }
-        output.append(output_record)
+    #     output_record = {
+    #         'recordId': record['recordId'],
+    #         'result': 'Ok',
+    #         'data': base64.b64encode(json.dumps(reading).encode('UTF-8'))
+    #     }
+    #     output.append(output_record)
 
-    print('Processed {} records.'.format(len(event['records'])))
+    # print('Processed {} records.'.format(len(event['records'])))
 
     # connect to sqs
     # sqs = boto3.client('sqs')
@@ -32,4 +32,4 @@ def start(event, context):
     #     MessageBody = json_data
     # )
     
-    return {'records': output}
+    # return {'records': output}
