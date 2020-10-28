2c04801256308c663713dcd6a06754b7674bc3d4
alial95 <alistair_j_cooper@hotmail.com>
2020-10-28 09:20:24 +0000

    added try catch to ds-extract

--- group-1/extract_data_stream/extract_data_stream.py
+++ group-1/extract_data_stream/extract_data_stream.py
@@ -2,27 +2,33 @@ import base64
 import json
 
 # perhaps use regex expressions to identify patterns in the data stream
+["26/10/2020 18:39", "Chichester", "Angela Labrecque", " Speciality Tea - Earl Grey - 1.30", "1.30", "CARD", "5468283889313827"]
 
 def start(event, context):
     
-    output = []
-
+    raw_transactions = []
     for record in event['Records']:
         print(record)
-        decoded_data = base64.b64decode(record['kinesis']['data']).decode('utf-8')
-        print(decoded_data)
-        # reading = json.loads(decoded_data)
-        # print(reading)
+        try:
+            decoded_data = base64.b64decode(record['kinesis']['data'])
+        except Exception as error:
+            print(json.dumps({'Error Message': error}))
+        try:
+            transaction = {
+                'date': decoded_data[0],
+                'location': decoded_data[1],
+                'customer_name': decoded_data[2],
+                'basket': decoded_data[3],
+                'total': decoded_data[4]
+            }   
+            raw_transactions.append(transaction)
+        except Exception as error:
+            print(json.dumps({'Error Message': error}))
+        json_data = json.dumps(raw_transactions)
+        print(json_data)
+        # print(json.dumps({'Record processed': }))    
 
-    #     output_record = {
-    #         'recordId': record['recordId'],
-    #         'result': 'Ok',
-    #         'data': base64.b64encode(json.dumps(reading).encode('UTF-8'))
-    #     }
-    #     output.append(output_record)
-
-    # print('Processed {} records.'.format(len(event['records'])))
-
+        
     # connect to sqs
     # sqs = boto3.client('sqs')
     # # send the message
